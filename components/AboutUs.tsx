c024a415e867386d8dab72b05d6923fbcacb4230
brayanriuge <145686424+brayanriunge@users.noreply.github.com>
2026-07-22 21:13:57 +0300

    adding a section

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -5,10 +5,10 @@ import CEO from "@/public/ceo.jpg";
 export default function AboutUs() {
   return (
     <section
-      className="min-h-[60vh] md:min-h-[70vh] lg:h-full py-8 px-4  bg-[#ffff] bg-[url('/world.png')] bg-cover bg-center bg-no-repeat "
+      className="min-h-[60vh] md:min-h-[70vh] lg:h-full py-8 px-4  bg-gray-200 bg-[url('/world.png')] bg-cover bg-center bg-no-repeat "
       id="aboutus"
     >
-      <div className="flex flex-col md:flex-row lg:flex-row justify-items-center  mx-auto py-8 px-8 gap-4 w-full">
+      <div className="flex flex-col md:flex-row lg:flex-row justify-items-center container mx-auto px-4 sm:px-6 lg:px-8  mx-auto py-8 px-8 gap-4 w-full">
         <div className="flex flex-col mx-auto justify-content gap-4 w-full ">
           <h1 className="text-[#f64a86] font-bold text-[20px] mb-4">
             About Us
@@ -35,8 +35,28 @@ export default function AboutUs() {
             alt="image"
             height={500}
             width={500}
-            className="h-full  w-full rounded-md "
+            className="h-90  w-full rounded-md aspect-3/2 "
           />
+        </div>
+      </div>
+      {/* what we do */}
+      <div className=" container mx-auto px-4 sm:px-6 lg:px-8  h-full ">
+        <div className="flex items-center justify-center">
+          <h1 className="text-[#f64a86] font-bold text-[28px] mb-4">
+            What Do We{" "}
+          </h1>
+        </div>
+        <div className="grid md:grid-col-2 lg:grid-col-3 mx-auto">
+          <div className="bg-[#5b4bf4] rounded-md rounded-br-4xl">
+            <h2 className="font-bold text-[20px] text-white">
+              Global Commodities Trading
+            </h2>
+            <p className="text-base text-white ">
+              Tackling social challanges demands bold leadership across
+              foundations, businesses, and government leaders willing to rethink
+              the systems and reletionships that shape our world.
+            </p>
+          </div>
         </div>
       </div>
     </section>
