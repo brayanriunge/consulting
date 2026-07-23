84681583d5efcdc775a5bf6734958e49b0e5dc81
brayanriuge <145686424+brayanriunge@users.noreply.github.com>
2026-07-23 18:52:18 +0300

    case component

---
+++ components/Cases.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Cases() {
+  return (
+    <section className="min-h-[60vh] md:min-h-[70vh] lg:h-full py-8 px-4  bg-gray-200  ">
+      <div className="flex items-center justify-center">
+        <h1 className="text-[#f64a86] font-bold text-[28px] mb-4">
+          Completed Cases
+        </h1>
+      </div>
+    </section>
+  );
+}
