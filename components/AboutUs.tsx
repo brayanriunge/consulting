ab7e2a2e141d368aabb884a67bd1fa5ff5e6faec
brayanriuge <145686424+brayanriunge@users.noreply.github.com>
2026-07-23 13:46:26 +0300

    About us component

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import CEO from "@/public/ceo.jpg";
+import Business from "@/public/business.jpg";
+import Finance from "@/public/finance.jpg";
 
 export default function AboutUs() {
   return (
@@ -8,7 +10,7 @@ export default function AboutUs() {
       className="min-h-[60vh] md:min-h-[70vh] lg:h-full py-8 px-4  bg-gray-200 bg-[url('/world.png')] bg-cover bg-center bg-no-repeat "
       id="aboutus"
     >
-      <div className="flex flex-col md:flex-row lg:flex-row justify-items-center container mx-auto px-4 sm:px-6 lg:px-8  mx-auto py-8 px-8 gap-4 w-full">
+      <div className="flex flex-col md:flex-row lg:flex-row justify-items-center container sm:px-6 lg:px-8  mx-auto py-8 px-8 gap-4 w-full">
         <div className="flex flex-col mx-auto justify-content gap-4 w-full ">
           <h1 className="text-[#f64a86] font-bold text-[20px] mb-4">
             About Us
@@ -46,18 +48,58 @@ export default function AboutUs() {
             What Do We{" "}
           </h1>
         </div>
-        <div className="grid md:grid-col-2 lg:grid-col-3 mx-auto">
-          <div className="bg-[#5b4bf4] rounded-md rounded-br-4xl">
-            <h2 className="font-bold text-[20px] text-white">
-              Global Commodities Trading
-            </h2>
-            <p className="text-base text-white ">
-              Tackling social challanges demands bold leadership across
-              foundations, businesses, and government leaders willing to rethink
-              the systems and reletionships that shape our world.
-            </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto justify-cente">
+          <div className="bg-[#301fce] flex flex-col items-center  rounded-md rounded-br-[100px] ">
+            <div className="m-8">
+              <h2 className="font-bold text-[20px] text-white p-4">
+                Global Commodities Trading
+              </h2>
+              <p className="text-base text-white p-4 ">
+                Tackling social challanges demands bold leadership across
+                foundations, businesses, and government leaders willing to
+                rethink the systems and reletionships that shape our world.
+              </p>
+              <p className="text-white p-4 flex flex-row gap-2 ">
+                Learn More <span> {">"}</span>
+              </p>
+            </div>
           </div>
-          <div></div>
+          <div className="flex flex-col justify-content">
+            <div className="p-4">
+              <Image
+                src={Business}
+                alt="business"
+                className="w-100 h-50 rounded-md mb-4"
+              />
+              <h2 className="text-black font-regular text-[20px] mb-4">
+                Local Business Opportunities
+              </h2>
+              <p className="text-base text-black text-[16px]">
+                The intiative significantly enhance the company`s planning and
+                exection capabilities. Recognizing the demands of a
+                technology-driven era, thet understood that modernizing their
+                accounting systems was essential for long time access.
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col justify-content">
+            <div className="p-4">
+              <Image
+                src={Finance}
+                alt="finance"
+                className="w-100 h-50 rounded-md mb-4"
+              />
+              <h2 className="text-black font-regular text-[20px] mb-4">
+                Financial Services
+              </h2>
+              <p className="text-base text-black text-[16px]">
+                Many companies shy away from the term `turnaround consulting`
+                due to its association to failure. In reliaty, it signifies
+                resilience a strategic step towards recovery, renewed focus, and
+                sustainable success.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
