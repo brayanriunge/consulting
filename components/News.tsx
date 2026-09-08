13b108d1a2c7f3f3d9d0eacb41e0c8fee72e0197
brayanriuge <145686424+brayanriunge@users.noreply.github.com>
2026-09-08 18:41:37 +0300

    making client feedback responsive

--- components/News.tsx
+++ components/News.tsx
@@ -70,7 +70,7 @@ export default function News() {
           Clients Feedback
         </h1>
       </div>
-      <div className="flex flex-row  items-stretch justify-center gap-6 mb-4">
+      <div className="flex flex-col md:flex-row lg:flex-row  items-stretch justify-center gap-6 mb-4">
         {/* first div */}
         <div className="flex flex-col">
           <div className="flex flex-col bg-gray-300 gap-4 rounded">
